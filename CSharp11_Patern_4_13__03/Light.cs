a7794afc40603847867c61c7d05c293d4a143261
MaksimMyson <maksimmyson33@gmail.com>
2024-03-13 20:37:45 +0200

    init comit

---
+++ CSharp11_Patern_4_13__03/Light.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp11_Patern_4_13__03
+{
+    internal class Light
+    {
+    }
+}
