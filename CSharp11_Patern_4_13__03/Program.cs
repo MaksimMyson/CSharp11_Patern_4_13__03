c3257d057e59649535b1afbd3f3d6ff0f40448ec
MaksimMyson <maksimmyson33@gmail.com>
2024-03-13 20:29:24 +0200

    Add project files.

---
+++ CSharp11_Patern_4_13__03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp11_Patern_4_13__03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
